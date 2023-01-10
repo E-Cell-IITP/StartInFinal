98d2c2c3ec69529f8b95741ebf9bf8b5fe2014fd
Saurabh Kandpal <saurabh_2001ee63@iitp.ac.in>
2023-01-10 14:39:11 +0530

    payment added

--- src/components/payment.js
+++ src/components/payment.js
@@ -44,6 +44,28 @@ import basicUpload from "../services/imageUploader";
   import * as Loader from 'react-loader-spinner';
   import { ToastContainer, toast } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
+
+// var axios = require('axios');
+var FormData = require('form-data');
+
+// var config = {
+//   method: 'post',
+//   url: 'https://api.imgur.com/3/upload',
+//   headers: { 
+//     'Authorization': 'Bearer 5eeae49394cd929e299785c8805bd168fc675280', 
+//     ...data.getHeaders()
+//   },
+//   data : data
+// };
+
+// axios(config)
+// .then(function (response) {
+//   console.log(JSON.stringify(response.data));
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
+
   
   
   const Payment = () => {
@@ -51,25 +73,45 @@ import basicUpload from "../services/imageUploader";
     const [category, setCategory] = useState(0);
     const [toggle,settoggle] = useState(true)
   
-    const handleUpload = (e) =>{
+    const handleUpload = async (e) =>{
         // e.target.value
-        console.log(e.target.files[0]);
-        basicUpload({
-            accountId: "FW25awF",
-            apiKey: "public_FW25awFBhDZhMzVWBDYaRuPTs4ne",
-            requestBody: new Blob( // Or: pass a 'file' object from an input element.
-              e.target.files
-            ),
-            querystring : {
-                fileName: `${teamName}_payment.jpg`,
-                fielNameVariablesEnabled: true,
-                tag: ["images/payment"]
-            }
-        }).then(
-          response => {console.log(`Success: ${JSON.stringify(response)}`);
-            console.log(response.fileUrl)},
-          error => console.error(error)
-        );
+        console.log(e.target.files[0])
+        const file = e.target.files[0];
+        let data = new FormData();
+        data.append('image', e.target.files[0]);
+
+         const result =  await axios.post('https://api.imgur.com/3/upload',data, 
+         {headers: { 
+            'Authorization': 'Bearer 5eeae49394cd929e299785c8805bd168fc675280',
+            'Content-Type' : 'multipart/form-data',
+
+          }});
+          console.log(result.data.data.link);
+          let item = {token : localStorage.getItem('token'), link : result.data.data.link}
+          const response = await axios.post('https://ecell-startin-backend.onrender.com/users/payment-detail',
+          item,
+          {headers:{
+            "Content-Type":'application/json',
+            "Accept":'application/json',
+          }}
+          )
+          console.log(response);
+        // basicUpload({
+        //     accountId: "FW25awF",
+        //     apiKey: "public_FW25awFBhDZhMzVWBDYaRuPTs4ne",
+        //     requestBody: new Blob( // Or: pass a 'file' object from an input element.
+        //       e.target.files
+        //     ),
+        //     querystring : {
+        //         fileName: `${teamName}_payment.jpg`,
+        //         fielNameVariablesEnabled: true,
+        //         tag: ["images/payment"]
+        //     }
+        // }).then(
+        //   response => {console.log(`Success: ${JSON.stringify(response)}`);
+        //     console.log(response.fileUrl)},
+        //   error => console.error(error)
+        // );
     }
 
     const showToastMessage = (x) => {
